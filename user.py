30260c1e6693785efbc561caeb06a5e555195fb1
LiONXXII <nadya03081997@mail.ru>
2026-08-12 01:56:46 +0300

    Добавлены комментарии

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 class User:
 
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        self.first_name = first_name    # Имя
+        self.last_name = last_name      # Фамилия
 
     def print_first_name(self):
         print(self.first_name)
