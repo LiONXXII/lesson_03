c9f300dc12b097db88942e87dcff8cad226c2ea8
LiONXXII <nadya03081997@mail.ru>
2026-08-12 13:36:06 +0300

    Дописала код

--- lesson_3_task_3.py
+++ lesson_3_task_3.py
@@ -9,4 +9,7 @@ mail = Mailing(to_address, from_address, 350.50, "TRACK123456")
 
 print(f"Отправление {mail.track} из "
       f"{mail.from_address.index}, {mail.from_address.city}, "
-      f"{mail.from_address.street}, {mail.from_address.house}")
+      f"{mail.from_address.street}, {mail.from_address.house} - {mail.from_address.apartment} "
+      f"в {mail.to_address.index}, {mail.to_address.city}, "
+      f"{mail.to_address.street}, {mail.to_address.house} - {mail.to_address.apartment}. "
+      f"Стоимость {mail.cost} рублей.")
